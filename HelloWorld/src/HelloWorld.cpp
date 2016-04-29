e0348ca30f7869eaa42598b57a978f882f9891eb
gzq <gaozq1992@163.com>
2016-04-29 02:27:56 -0700

    this is a test

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : gzq
+// Version     :
+// Copyright   : nju
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints !!!Hello World!!!
+	return 0;
+}
