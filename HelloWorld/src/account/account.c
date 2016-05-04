2d3be193273c07491b5dc44450a1a5d6f32f5919
gzq <gaozq1992@163.com>
2016-05-04 01:33:36 -0700

    Using semaphore to write bank account problem

--- HelloWorld/src/account/account.c
+++ HelloWorld/src/account/account.c
@@ -10,14 +10,18 @@ Account* create_account(int code,double balance)
 	assert(a != NULL);
 	a->code = code;
 	a->balance = balance;
-	pthread_mutex_init(&a->mutex,NULL);
+	//pthread_mutex_init(&a->mutex,NULL);
+	//pthread_rwlock_init(&a->rwlock,NULL);
+	sem_init(&a->sem,0,1);
 	return a;
 }
 void destroy_account(Account* a)
 {
 	assert(a != NULL);
 	
-	pthread_mutex_destroy(&a->mutex);
+	//pthread_mutex_destroy(&a->mutex);
+	//pthread_rwlock_destroy(&a->rwlock);
+	sem_destroy(&a->sem);
 	free(a);
 }
 
@@ -25,11 +29,14 @@ double withdraw(Account* a,double amt)
 {
 	assert(a != NULL);
 	
-	pthread_mutex_lock(&a->mutex);
-		
+	//pthread_mutex_lock(&a->mutex);
+	//pthread_rwlock_wrlock(&a->rwlock);	
+	sem_wait(&a->sem);
 	if(amt < 0 || amt > a->balance)
 	{
-		pthread_mutex_unlock(&a->mutex);
+		//pthread_mutex_unlock(&a->mutex);
+		//pthread_rwlock_unlock(&a->rwlock);
+		sem_post(&a->sem);
 		return 0.0;
 	}
 	double balance = a->balance;
@@ -37,7 +44,9 @@ double withdraw(Account* a,double amt)
 	balance -= amt;
 	a->balance = balance;
 	
-	pthread_mutex_unlock(&a->mutex);
+	//pthread_mutex_unlock(&a->mutex);
+	//pthread_rwlock_unlock(&a->rwlock);
+	sem_post(&a->sem);
 	return amt;
 }
 
@@ -45,10 +54,15 @@ double deposit(Account* a,double amt)
 {
 	assert(a != NULL);
 	
-	pthread_mutex_lock(&a->mutex);
+	//pthread_mutex_lock(&a->mutex);
+	//pthread_rwlock_wrlock(&a->rwlock);
+	sem_wait(&a->sem);
 	
 	if(amt < 0)
 	{
+		//pthread_mutex_unlock(&a->mutex);
+		//pthread_rwlock_unlock(&a->rwlock);
+		sem_post(&a->sem);
 		return 0.0;
 	}
 	double balance = a->balance;
@@ -56,7 +70,9 @@ double deposit(Account* a,double amt)
 	balance += amt;
 	a->balance = balance;
 	
-	pthread_mutex_unlock(&a->mutex);
+	//pthread_mutex_unlock(&a->mutex);
+	//pthread_rwlock_unlock(&a->rwlock);
+	sem_post(&a->sem);
 	return amt;
 }
 
@@ -64,11 +80,14 @@ double get_balance(Account* a)
 {
 	assert(a != NULL);
 	
-	pthread_mutex_lock(&a->mutex);
-	
+	//pthread_mutex_lock(&a->mutex);
+	//pthread_rwlock_rdlock(&a->rwlock);
+	sem_wait(&a->sem);
 	double balance = a->balance;
 	
-	pthread_mutex_unlock(&a->mutex);
+	//pthread_mutex_unlock(&a->mutex);
+	//pthread_rwlock_unlock(&a->rwlock);
+	sem_post(&a->sem);
 	return balance;
 }
 
---
+++ HelloWorld/src/semaphore/sem_test.c
@@ -0,0 +1,64 @@
+#include<pthread.h>
+#include<semaphore.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+//define semaphore of threads
+sem_t sem1;
+sem_t sem2;
+
+void* a_fn(void* arg)
+{
+	sem_wait(&sem1);
+	printf("thread a running\n");
+	
+	return (void*) 0;
+}
+
+void* b_fn(void* arg)
+{
+	sem_wait(&sem2);
+	printf("thread b running\n");
+	//Release thread a to continue running	
+	sem_post(&sem1);
+	return (void*) 0;
+}
+
+void* c_fn(void* arg)
+{
+	printf("thread c running\n");
+	//Release thread b to continue running
+	sem_post(&sem2);
+	return (void*) 0;
+}
+
+int main()
+{
+	pthread_t a,b,c;
+	
+	sem_init(&sem1,0,0);
+	sem_init(&sem2,0,0);
+	
+	int err;
+	if((err = pthread_create(&a,NULL,a_fn,(void*)0)) != 0)
+	{
+		perror("pthread create error");
+	}
+	if((err = pthread_create(&b,NULL,b_fn,(void*)0)) != 0)
+	{
+		perror("pthread create error");
+	}
+	if((err = pthread_create(&c,NULL,c_fn,(void*)0)) != 0)
+	{
+		perror("pthread create error");
+	}
+	
+	pthread_join(a,NULL);
+	pthread_join(b,NULL);
+	pthread_join(c,NULL);
+	
+	sem_destroy(&sem1);
+	sem_destroy(&sem2);
+	return 0;
+}
+
